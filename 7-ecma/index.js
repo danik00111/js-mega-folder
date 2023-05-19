927926705cae259f376654c44343ecd9de50d4bb
danik00111 <kremenskiydaniil@gmail.com>
2023-05-19 20:11:40 +0300

    ++7-ecma

---
+++ 7-ecma/index.js
@@ -0,0 +1,21 @@
+//. f && f === f
+//. T && f === f
+//. f && T === f
+//. T && T === T
+//. f || f === f
+//. T || f === T
+//. f || T === T
+//. T || T === T
+//. !T === f
+//. !f === T
+//. f ^ f === f
+//. T ^ f === T
+//. f ^ T === T
+//. T ^ T === f
+
+const pr = prompt('What\'s JavaScript\'s official name?')
+if (p === 'ECMAScript') {
+  alert('Indeed! It\'s"ECMAScript".')
+} else {
+  alert('No. It\'s "ECMAScript".')
+}
