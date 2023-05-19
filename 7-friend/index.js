3ea8f69ba0bfb5d9d89b9ea3fc46c34b8a4f4866
danik00111 <kremenskiydaniil@gmail.com>
2023-05-19 20:43:01 +0300

    ++7-friend

---
+++ 7-friend/index.js
@@ -0,0 +1,10 @@
+const isOnline = true;
+const isFriend = true;
+const isDnd = false;
+
+console.log((isOnline && isFriend && !isDnd) ? true : false);
+// if (isOnline && isFriend && !isDnd) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
