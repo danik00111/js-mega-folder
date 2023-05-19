434b31b9c0fa733c3bb2c936b5fe69f70dcd1276
danik00111 <kremenskiydaniil@gmail.com>
2023-05-19 20:25:51 +0300

    **7-ecma

--- 7-ecma/scr.js
+++ 7-ecma/scr.js
@@ -14,8 +14,9 @@
 //. T ^ T === f
 
 const pr = prompt('What\'s JavaScript\'s official name?')
-if (p === 'ECMAScript') {
-  alert('Indeed! It\'s"ECMAScript".')
-} else {
-  alert('No. It\'s "ECMAScript".')
-}
+// if (pr === 'ECMAScript') {
+//   alert('Indeed! It\'s "ECMAScript".')
+// } else {
+//   alert('No. It\'s "ECMAScript".')
+// }
+alert(`${(pr === 'ECMAScript') ? 'Indeed!' : 'No.'} It's "ECMAScript".`)
