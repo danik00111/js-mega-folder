9d38819bbd7d316cd9d6e63ff387be46c0b4ac65
danik00111 <kremenskiydaniil@gmail.com>
2023-05-24 19:23:01 +0300

    **8-dz

--- 8-dz/script.js
+++ 8-dz/script.js
@@ -2,12 +2,14 @@ document.querySelector('.button').addEventListener('click', () => {
   let balance = parseFloat(document.querySelector('.balance').value);
   let payment = parseFloat(document.querySelector('.payment').value);
   alert(`Alright, that will be $${payment}. You currently have $${balance}.`)
-  if (balance < payment) {
-    alert("It seems that you're too broke to afford that.");
-  } else {
-    balance -= payment;
-    alert(`You paid $${payment} and now have $${balance}.`);
-    document.querySelector('.balance').value = balance;
-    document.querySelector('.payment').value = 0;
+  if (payment != 0) {
+    if (balance < payment) {
+      alert("It seems that you're too broke to afford that.");
+    } else {
+      balance -= payment;
+      alert(`You paid $${payment} and now have $${balance}.`);
+      document.querySelector('.balance').value = balance;
+      document.querySelector('.payment').value = 0;
+    }
   }
 })
