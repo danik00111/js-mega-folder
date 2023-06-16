891083122a003b116f347c5f3f0d76ae28217b2d
danik00111 <kremenskiydaniil@gmail.com>
2023-06-16 10:28:57 +0300

    h

--- a-dz/scr.js
+++ a-dz/scr.js
@@ -1,7 +1,7 @@
 let age;
 const MY_FORM = document.getElementById('form');
 const MY_INPUT = document.getElementById('input');
-const MY_BUTTON = document.getElementById('button');
+// const MY_BUTTON = document.getElementById('button');
 const IMG_BABY = document.querySelector('.images #baby');
 const IMG_KID = document.querySelector('.images #kid');
 const IMG_TEEN = document.querySelector('.images #teen');
@@ -13,43 +13,55 @@ const IMG_UFO = document.querySelector('.images #ufo');
 const IMG_DINO = document.querySelector('.images #dino');
 const IMG_ERROR = document.getElementById('error');
 
-MY_FORM.addEventListener('submit', (event) => {
-console.log('debug log');
-event.preventDefault(); // Prevent form submission
-age = parseInt(MY_INPUT.value);
-document.querySelectorAll('.images > img').forEach(img => img.classList.remove('anim'));
-switch (true) {
-  case (0 <= age && age < 3):
-    IMG_BABY.classList.add('anim');
-    break;
-  case (3 <= age && age < 12):
-    IMG_KID.classList.add('anim');
-    break;
-  case (12 <= age && age < 18):
-    IMG_TEEN.classList.add('anim');
-    break;
-  case (18 <= age && age < 25):
-    IMG_YA.classList.add('anim');
-    break;
-  case (25 <= age && age < 50):
-    IMG_ADULT.classList.add('anim');
-    break;
-  case (50 <= age && age < 100):
-    IMG_ELDER.classList.add('anim');
-    break;
-  case (100 <= age && age < 200):
-    IMG_DEAD.classList.add('anim');
-    break;
-  case (200 <= age && age < 500):
-    IMG_UFO.classList.add('anim');
-    break;
-  case (age >= 500):
-    IMG_DINO.classList.add('anim');
-    break;
-  default:
-    IMG_ERROR.classList.add('anim');
-    setTimeout(() => {
-      location.href = "https://youtu.be/5BZLz21ZS_Y";
-    }, 100);
+const doTheThing = () => {
+  age = parseInt(MY_INPUT.value);
+  document.querySelectorAll('.images > img').forEach(img => img.classList.remove('anim'));
+  switch (true) {
+    case (0 <= age && age < 3):
+      IMG_BABY.classList.add('anim');
+      break;
+    case (3 <= age && age < 12):
+      IMG_KID.classList.add('anim');
+      break;
+    case (12 <= age && age < 18):
+      IMG_TEEN.classList.add('anim');
+      break;
+    case (18 <= age && age < 25):
+      IMG_YA.classList.add('anim');
+      break;
+    case (25 <= age && age < 50):
+      IMG_ADULT.classList.add('anim');
+      break;
+    case (50 <= age && age < 100):
+      IMG_ELDER.classList.add('anim');
+      break;
+    case (100 <= age && age < 200):
+      IMG_DEAD.classList.add('anim');
+      break;
+    case (200 <= age && age < 500):
+      IMG_UFO.classList.add('anim');
+      break;
+    case (age >= 500):
+      IMG_DINO.classList.add('anim');
+      break;
+    default:
+      IMG_ERROR.classList.add('anim');
+      setTimeout(() => {
+        location.href = "https://youtu.be/5BZLz21ZS_Y";
+      }, 100);
+  }
 }
+
+// stovf :)
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.type === "attributes") {
+      doTheThing();
+    }
+  });
+});
+
+observer.observe(element, {
+  attributes: true //configure it to listen to attribute changes
 });
