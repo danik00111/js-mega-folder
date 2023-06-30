8ebfd8a762a7a79376e5963dcd933570a06342fd
danik00111 <kremenskiydaniil@gmail.com>
2023-06-30 19:25:10 +0300

    ++c-dz

---
+++ c-dz/index.js
@@ -0,0 +1,7 @@
+let programmingStudents = [
+  'Radchenko', 'Egor', 'Kostya', 'Gleb',
+  'Bozhko', 'Polina', 'Vod', 'Ilya',
+  'Myroslav', 'Hladchuk', 'Kirill', 'Daniil'];
+let programmingMentors = ['MorraMay', 'j', 'Dudenko'];
+let programmingClass = programmingMentors.concat(programmingStudents);
+console.log(programmingClass);
