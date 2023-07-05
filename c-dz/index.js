998fc8ff315dbc0e76d07889d146952076574e5a
danik00111 <kremenskiydaniil@gmail.com>
2023-07-05 19:28:10 +0300

    r

--- c-dz/index.js
+++ c-dz/index.js
@@ -1,10 +1,15 @@
 let programmingStudents = [
-  'Radchenko', 'Egor',
-  'Kostya',    'Gleb',
-  'Bozhko',    'Polina',
-  'Vod',       'Ilya',
-  'Myroslav',  'Hladchuk',
-  'Kirill',    'Daniil'];
+  'Radchenko', 'Egor',   'Kostya',
+  'Gleb',      'Bozhko', 'Polina',
+  'Vod',       'Ilya',   'Myroslav',
+  'Hladchuk',  'Kirill', 'Daniil'];
 let programmingMentors = ['MorraMay', 'j', 'Dudenko'];
 let programmingClass = programmingMentors.concat(programmingStudents);
 console.log(programmingClass);
+/*[
+  'MorraMay',  'j',      'Dudenko',
+  'Radchenko', 'Egor',   'Kostya',
+  'Gleb',      'Bozhko', 'Polina',
+  'Vod',       'Ilya',   'Myroslav',
+  'Hladchuk',  'Kirill', 'Daniil'
+]*/
---
+++ e-arrarrarrayrar/index.js
@@ -0,0 +1,29 @@
+// ['a', 'b', 'c'].join(' ') === 'a b c'
+// ['a', 'b', 'c'].join('-') === 'a-b-c'
+// ['a', 'b', 'c'].join(', ') === 'a, b, c'
+// 'a b c'.split(' ') === ['a', 'b', 'c']
+// 'a-b-c'.split('-') === ['a', 'b', 'c']
+// 'a, b, c'.split(', ') === ['a', 'b', 'c']
+// ['a', 'b', 'c'].indexOf('a') === 0
+// ['a', 'b', 'c'].indexOf('c') === 2
+// ['a', 'b', 'c'].indexOf('h') === -1
+// ['a', 'b', 'c'].includes('b') === true
+// ['a', 'b', 'c'].includes('d') === false
+
+/* const array = [];
+console.log(array); // []
+array.push(1);
+console.log(array); // [1]
+array.push(48);
+console.log(array); // [1, 48]
+array.pop();
+console.log(array); // [1] */
+
+/* splice(start)
+// видаляє все починаючи з елемента з індексом start
+splice(start, deleteCount)
+// видяляє deleteCount елементів починаючи з елемента start
+splice(start, deleteCount, item0)
+splice(start, deleteCount, item0, item1)
+splice(start, deleteCount, item0, item1, …)
+// також додає елемент(и) на місці вибуху */
