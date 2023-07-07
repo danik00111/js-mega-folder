d0c8c1a02c7610dea56ad12875490b3266cff430
danik00111 <kremenskiydaniil@gmail.com>
2023-07-07 19:11:27 +0300

    r

--- snippets.js
+++ snippets.js
@@ -58,3 +58,5 @@ export {
   maxOfArray,
   randInt
 };
+
+// export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
