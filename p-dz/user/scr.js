7f5c4351974cdc07b9a2d017669d2b849a671d7d
danik00111 <kremenskiydaniil@gmail.com>
2023-08-07 18:06:16 +0300

    boom

--- p-dz/user/scr.js
+++ p-dz/user/scr.js
@@ -11,4 +11,9 @@ document.querySelector('#butter').addEventListener('click', () => {
   let inputpas = document.querySelector('#password').value;
   let inputeml = document.querySelector('#email').value;
   document.querySelector('.output').innerHTML = user.Check(inputpas, inputeml);
+  document.querySelector('.output').classList.remove('right');
+  document.querySelector('.output').classList.remove('nulle');
+  if(user.Check(inputpas, inputeml)) {
+    document.querySelector('.output').classList.add('right');
+  }
 });
