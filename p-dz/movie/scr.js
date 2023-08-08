3a25958e33642821940d8e59ae04d4c4e9540e5d
danik00111 <kremenskiydaniil@gmail.com>
2023-08-08 14:46:23 +0300

    it working *victory dance*

--- p-dz/movie/scr.js
+++ p-dz/movie/scr.js
@@ -7,7 +7,7 @@ let movies = [{
   },
   Rating: 0.88, // 0 - 1, rotten - fresh
   BannerPath: './img/barbie-banner.jpeg',
-  RatingCheck: function(){
+  FreshnessCheck: function(){
     return this.Rating > 0.8;
   }
 },{
@@ -19,7 +19,18 @@ let movies = [{
   },
   Rating: 0.28, // 0 - 1, rotten - fresh
   BannerPath: './img/meg-2-the-trench-banner.jpeg',
-  RatingCheck: function(){
+  FreshnessCheck: function(){
     return this.Rating > 0.8;
   }
 }]
+
+for(let i=0;i<movies.length;i++) { // loop thorugh all movies objects
+  document.querySelector(`.movie:nth-child(${i+1}) .banner`).src = movies[i].BannerPath;
+  document.querySelector(`.movie:nth-child(${i+1}) .title`).innerHTML = movies[i].Title;
+  document.querySelector(`.movie:nth-child(${i+1}) .director`).innerHTML = movies[i].Director.Name;
+  document.querySelector(`.movie:nth-child(${i+1}) .director`).href = movies[i].Director.WikiLink;
+  document.querySelector(`.movie:nth-child(${i+1}) .year`).innerHTML = movies[i].Year;
+  document.querySelector(`.movie:nth-child(${i+1}) .percend`).innerHTML = Math.round((movies[i].Rating) * 100);
+  document.querySelector(`.movie:nth-child(${i+1}) .rating`).classList.add((movies[i].FreshnessCheck()) ? 'fresh' : 'rotten');
+  document.querySelector(`.movie:nth-child(${i+1}) .tomatometer`).src = `./img/tomatometer-${(movies[i].FreshnessCheck()) ? 'fresh' : 'rotten'}.svg`;
+}
