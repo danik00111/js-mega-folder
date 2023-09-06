8c9fa6cfda2ed6f07e31eafaa4824570418b054c
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-09-06 18:36:22 +0300

    ++3-dz

---
+++ vol2/3-dz/index.js
@@ -0,0 +1,47 @@
+let users = [{
+  name: 'Poly',
+  following: ['Mango', 'Kiwi'],
+  skills: [ 'lasereyes', 'invisibility' ],
+  balance: 9471,
+},{
+  name: 'Ajax',
+  following: [ ],
+  skills: [ 'timecontrol', 'flight' ],
+  balance: 6894,
+},{
+  name: 'Mango',
+  following: ['Ajax'],
+  skills: [ 'mindcontrol', 'lasereyes'],
+  balance: 2759,
+},{
+  name: 'Kiwi',
+  following: ['Mango', 'Poly', 'Ajax'],
+  skills: [ 'flight', 'mindcontrol' ],
+  balance: 4025,
+}]
+
+const sumArray = (r) => {
+  let sum = 0;
+  r.forEach(i=>{sum += i;});
+  return sum;
+}
+const totalBalance = sumArray(users.map(x => x.balance));
+console.log(totalBalance);
+
+/////////////////////////////////////////////////////////////////////
+
+const findFollowers = (arr, username) => arr.filter(x => x.following.includes(username)).map(x => x.name);
+console.log(findFollowers(users, 'Ajax'));
+
+/////////////////////////////////////////////////////////////////////
+
+const sortByFollows = (arr) => arr.sort((a, b) => b.following.length - a.following.length).map(x => x.name);
+console.log(sortByFollows(users));
+
+/////////////////////////////////////////////////////////////////////
+
+import { noDupesAndSort } from "../../snippets/index.js";
+
+let allSkills = noDupesAndSort([].concat(...(users.map(x => x.skills))), []);
+
+console.log(allSkills);
