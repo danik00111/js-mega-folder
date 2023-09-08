6f3622e2c06ebbac676e9abb0f41852b5df7091c
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-09-08 19:32:02 +0300

    Cursed

--- vol2/3-dz/index.js
+++ vol2/3-dz/index.js
@@ -1,47 +1,77 @@
 let users = [{
-  name: 'Poly',
-  following: ['Mango', 'Kiwi'],
-  skills: ['lasereyes', 'invisibility'],
-  balance: 9471,
-},{
-  name: 'Ajax',
-  following: [ ],
-  skills: ['timecontrol', 'flight'],
-  balance: 6894,
-},{
-  name: 'Mango',
-  following: ['Ajax'],
-  skills: ['mindcontrol', 'lasereyes'],
-  balance: 2759,
-},{
-  name: 'Kiwi',
-  following: ['Mango', 'Poly', 'Ajax'],
-  skills: ['flight', 'mindcontrol'],
-  balance: 4025,
+  name: 'Poly'                                                                                                        ,
+
+  following:                                                                                                          [
+    'Mango'                                                                                                           ,
+    'Kiwi'
+                                                                                                                     ],
+  skills:                                                                                                             [
+    'lasereyes'                                                                                                       ,
+    'invisibility'
+                                                                                                                     ],
+  balance: 9471                                                                                                       ,
+}                                                                                                                     ,
+{
+  name: 'Ajax'                                                                                                        ,
+
+  following: 
+                                                                                                                    [ ]                                                                                                      
+                                                                                                                      ,
+  skills:                                                                                                             [
+    'timecontrol'                                                                                                     ,
+    'flight'
+                                                                                                                     ],
+  balance: 6894                                                                                                       ,
+}                                                                                                                     ,
+{
+  name: 'Mango'                                                                                                       ,
+
+  following:                                                                                                          [
+    'Ajax'
+                                                                                                                     ],
+  skills:                                                                                                             [
+    'mindcontrol'                                                                                                     ,
+    'lasereyes'
+                                                                                                                     ],
+  balance: 2759                                                                                                       ,
+}                                                                                                                     ,
+{
+  name: 'Kiwi'                                                                                                        ,
+  
+  following:                                                                                                          [
+    'Mango'                                                                                                           ,
+    'Poly'                                                                                                            ,
+    'Ajax'
+                                                                                                                     ],
+  skills:                                                                                                             [
+    'flight'                                                                                                          ,
+    'mindcontrol'
+                                                                                                                     ],
+  balance: 4025                                                                                                       ,
 }]
 
-const sumArray = (r) => {
-  let sum = 0;
-  r.forEach(i=>{sum += i;});
-  return sum;
-}
-const totalBalance = sumArray(users.map(x => x.balance));
-console.log(totalBalance); // 23149
+const sumArray = (r)                                                                                               => {
+  let sum = 0                                                                                                         ;
+  r.forEach(i=>{sum += i})                                                                                            ;
+  return sum                                                                                                          ;
+                                                                                                                      }
+const totalBalance = sumArray(users.map(x => x.balance))                                                              ;
+console.log(totalBalance) /* 23149 */                                                                                 ;
 
 /////////////////////////////////////////////////////////////////////
 
-const findFollowers = (arr, username) => arr.filter(x => x.following.includes(username)).map(x => x.name);
-console.log(findFollowers(users, 'Ajax')); // ['Mango', 'Kiwi']
+const findFollowers = (arr, username) => arr.filter(x => x.following.includes(username)).map(x => x.name)             ;
+console.log(findFollowers(users, 'Ajax')); /* ['Mango', 'Kiwi'] */
 
 /////////////////////////////////////////////////////////////////////
 
-const sortByFollows = (arr) => arr.sort((a, b) => b.following.length - a.following.length).map(x => x.name);
-console.log(sortByFollows(users)); // ['Kiwi', 'Poly', 'Mango', 'Ajax']
+const sortByFollows = (arr) => arr.sort((a, b) => b.following.length - a.following.length).map(x => x.name)           ;
+console.log(sortByFollows(users)) /* ['Kiwi', 'Poly', 'Mango', 'Ajax'] */                                             ;
 
 /////////////////////////////////////////////////////////////////////
 
-import { noDupesAndSort } from "../../snippets/index.js";
+import { noDupesAndSort } from "../../snippets/index.js"                                                              ;
 
-let allSkills = noDupesAndSort([].concat(...(users.map(x => x.skills))), []);
+let allSkills = noDupesAndSort([].concat(...(users.map(x => x.skills))), [])                                          ;
 
-console.log(allSkills); // ['flight', 'invisibility', 'lasereyes', 'mindcontrol', 'timecontrol']
+console.log(allSkills) /* ['flight', 'invisibility', 'lasereyes', 'mindcontrol', 'timecontrol' ] */                   ;
