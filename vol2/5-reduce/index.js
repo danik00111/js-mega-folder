65be587a71ded2930fb7d39d36b576859c06f944
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-09-09 12:05:58 +0300

    +arryr

--- vol2/5-reduce/index.js
+++ vol2/5-reduce/index.js
@@ -20,3 +20,60 @@ const posts = [{
   message: ['method', 'send', 'help'],
 }]
 console.log(posts.reduce((a, v) => a + v.likes, 0));
+
+const usres = [{
+  name: 'Kiwi',
+  lastLogin: 1,
+},{
+  name: 'Poly',
+  lastLogin: 5,
+},{
+  name: 'Ajax',
+  lastLogin: 858, // ajax went to get milk for his kids :sob:
+},{
+  name: 'Mango',
+  lastLogin: 3,
+}]
+
+usres.sort((a,b) => a.lastLogin - b.lastLogin);
+
+console.log(usres);
+
+let arryr = [2, 6, 3, 7, 4, 0, 11, 5, 23, 9];
+
+const filterMod2 = (arr) => {
+  let returnValue = [];
+  for(let i=0; i < arr.length; i++) { if(arr[i] % 2 === 0) returnValue.push(arr[i]) };
+  return returnValue;
+}
+
+const doubleNumbersInArray = (arr) => {
+  let returnValue = [];
+  for(let i=0; i < arr.length; i++) { returnValue.push(arr[i] * 2)};
+  return returnValue;
+}
+
+const invertArray = (arr) => {
+  let returnValue = [];
+  for(let i=arr.length - 1; i >= 0; i--) { returnValue.push(arr[i])};
+  return returnValue;
+}
+
+console.log(
+  invertArray(
+    doubleNumbersInArray(
+      filterMod2(
+        arryr
+      )
+    )
+  )
+);
+
+// or you could just:
+
+console.log(
+  arryr
+  .filter(x => x % 2 === 0)
+  .map(x => x * 2)
+  .reverse()
+);
