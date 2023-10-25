0f878ed7ca509cefe328def7ea9d6154f970413a
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-10-25 17:56:15 +0300

    optimise switch/case with a helper func

--- vol2/8-slidre/script.js
+++ vol2/8-slidre/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // just a safeguard
   const slider = document.querySelector(".slider");
   const prevBtn = document.getElementById("prev");
   const nextBtn = document.getElementById("next");
@@ -11,21 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const updateDots = () => {
     dots.forEach((dot, index) => { dot.classList.toggle("active", index === currentIndex); });
     switch(currentIndex) {
-      case 0:
-        sliderContainer.classList.add('lightred');
-        sliderContainer.classList.remove('lightblue');
-        sliderContainer.classList.remove('lightyellow');
-      break; case 1:
-        sliderContainer.classList.remove('lightred');
-        sliderContainer.classList.add('lightblue');
-        sliderContainer.classList.remove('lightyellow');
-      break; case 2:
-        sliderContainer.classList.remove('lightred');
-        sliderContainer.classList.remove('lightblue');
-        sliderContainer.classList.add('lightyellow');
-      break;
+      case 0: classesFunc('lightred'); break;
+      case 1: classesFunc('lightblue'); break;
+      case 2: classesFunc('lightyellow'); break;
     }
   }
+  const classesFunc = (x) => {
+    sliderContainer.classList.forEach((className) => {
+      if(className !== 'slider-container') { sliderContainer.classList.remove(className); }
+      sliderContainer.classList.add(x);
+    });
+  };
   const goToNextSlide = () => { currentIndex = (currentIndex + 1) % slides.length; showSlide(currentIndex); updateDots(); }
   const goToPrevSlide = () => { currentIndex = (currentIndex - 1 + slides.length) % slides.length; showSlide(currentIndex); updateDots(); }
   prevBtn.addEventListener("click", goToPrevSlide);
