be53a165c73dec7f6b114eeba35d4859635f169a
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-10-29 17:16:43 +0200

    i just misplaced the array brackets silly me hahah

--- vol2/9-elevtaor/scr.js
+++ vol2/9-elevtaor/scr.js
@@ -60,10 +60,21 @@ const elevTo = (num) => {
 let floorCalls = [
   [false,false,false,false,false,false,false,false,false,false],
   [false,false,false,false,false,false,false,false,false,false]
-]
+];
 let elevDir = null;
-
 const elevFunc = () => {
+  elevActualFunc();
+  if(document.querySelector(`.elev-button:nth-child(${floor})`).classList.contains('lid-up')) document.querySelector(`.elev-button:nth-child(${floor})`).classList.remove('lid-up');
+  console.log(floorCalls[0][floor - 1]);
+  if(floorCalls[0][floor-1] === true) {
+    floorCalls[0][floor-1] = false;
+    queue.splice(queue.indexOf(floor), 1);
+    // Open the doors to pick up the passanger...
+  };
+  console.log(floorCalls[0][floor - 1]);
+  queueOutput.innerHTML = queue;
+};
+const elevActualFunc = () => {
   updateFloorCalls();
   console.log(isGoingUp());
   console.log(isGoingDown());
@@ -73,69 +84,47 @@ const elevFunc = () => {
   console.log(isGoingUpOutside());
   console.log(isGoingDownInside());
   console.log(isGoingDownOutside());
-  {
-    // // if (floorCalls[0].every(x => !x) && floorCalls[0].every(x => x === false)) { console.log('Elevator is idle'); return;}
-    // if(isGoingUpInside() || isGoingDownInside()) {
-      //   console.log('There are lid up buttons inside the elev.')
-      //   if(init) {
-        //     if(isGoingUpInside()) { console.log('ELEVATOR IS MOVING UP'); init = false; floorUp(); return; };
-  //     if(isGoingDownInside()) { console.log('ELEVATOR IS MOVING DOWN'); init = false; floorDown(); return; }
-  //     console.log('ELEVATOR IS IDLE'); return;
-  //   }
-  //   if(elevDir&&isGoingUpInside()) { console.log('ELEVATOR IS MOVING UP'); floorUp(); return; };
-  //   if((!elevDir)&&isGoingDownInside()) { console.log('ELEVATOR IS MOVING DOWN'); floorDown(); return; }
-  //   console.log('ELEVATOR IS IDLE'); return;
-  // }
-  // console.log('There are NO lid up buttons inside the elev.')
-  // if (isGoingUpOutside() && isGoingDownOutside()) {
-    //   if (elevDir) { floorUp(); console.log('ELEVATOR IS MOVING UP'); } else { floorDown(); console.log('ELEVATOR IS MOVING DOWN'); }
-    //   return;
-    // }
-    // if((!isGoingUpOutside()) && (!isGoingDownOutside())) { console.log('ELEVATOR IS IDLE'); return;}
-    // console.log(`ELEVATOR IS MOVING ${isGoingUpOutside()?'UP':'DOWN'}`);
-    // elevDir = isGoingUpOutside();
-    // if(isGoingUpOutside()){floorUp(); return;}
-    // if(isGoingDownOutside()){floorDown(); return;}
-  }; // commented out code, folded
-  {
-  // if(elevDir === null) { // special case for first elev movement
-  //   console.log('Null.')
-  //   if(litButtonsInside()) { // prioritize requests from inside the elev
-  //     console.log('Indied,')
-  //     if(isGoingUpInside()) { floorUp(); elevDir = true; console.log('Elevator going up (in, null)'); return; };
-  //     if(isGoingDownInside()) { floorDown(); elevDir = false; console.log('Elevator going down (in, null)'); return; };
-  //   };
-  //   console.log('Not,')
-  //   if(isGoingUpOutside()) { floorUp(); elevDir = true; console.log('Elevator going up (out, null)'); return; };
-  //   if(isGoingDownOutside()) { floorDown(); elevDir = false; console.log('Elevator going down (out, null)'); return; };
-  //   console.log('Elevator idle (null)')
-  //   return;
-  // };
-  // if(litButtonsInside) { // prioritize requests from inside the elev
-  //   // Code...
-  //   return;
-  // };
-  }; // second attepmrt 2 and i am now already brugh screw this sgjt anymoer
-}
+  if(elevDir === null) { // special case for first elev movement
+    if(litButtonsInside()) { // prioritize requests from inside the elev
+      if(isGoingUpInside()) { floorUp(); elevDir = true; console.log('Elevator going up (in, null)'); return; };
+      if(isGoingDownInside()) { floorDown(); elevDir = false; console.log('Elevator going down (in, null)'); return; };
+    };
+    if(isGoingUpOutside()) { floorUp(); elevDir = true; console.log('Elevator going up (out, null)'); return; };
+    if(isGoingDownOutside()) { floorDown(); elevDir = false; console.log('Elevator going down (out, null)'); return; };
+    console.log('Elevator idle (null)');
+    return;
+  };
+  if(litButtonsInside()) { // prioritize requests from inside the elev
+    if((isGoingUpInside())&&(isGoingDownInside())) {
+      if(elevDir) {floorUp(); console.log('Elevator going up (in, both)'); elevDir = true; return;}
+      else {floorDown(); console.log('Elevator going down (in, both)'); elevDir = false; return;};
+    };
+    if((isGoingUpInside())) {floorUp(); console.log('Elevator going up (in)'); elevDir = true; return;};
+    if((isGoingDownInside())) {floorDown(); console.log('Elevator going down (in)'); elevDir = false; return;};
+  };
+  if((isGoingUpOutside())&&(isGoingDownOutside())) {
+    if(elevDir) {floorUp(); console.log('Elevator going up (out, both)'); elevDir = true; return;}
+    else {floorDown(); console.log('Elevator going down (out, both)'); elevDir = false; return;};
+  };
+  if(isGoingUpOutside()) {floorUp(); console.log('Elevator going up (out)'); elevDir = true; return;};
+  if(isGoingDownOutside()) {floorDown(); console.log('Elevator going down (out)'); elevDir = false; return;};
+  console.log('Elevator idle');
+};
 
 const isGoingUp = () => (isGoingUpInside() || isGoingUpOutside());
 const isGoingDown = () => (isGoingDownInside() || isGoingDownOutside());
 const litButtonsInside = () => (isGoingUpInside() || isGoingDownInside());
 const litButtonsOutside = () => (isGoingUpOutside() || isGoingDownOutside());
-const isGoingUpInside = () => [...floorCalls[1].splice(floor-1)].some(x => x);
-const isGoingUpOutside = () => [...floorCalls[0].splice(floor-1)].some(x => x);
-const isGoingDownInside = () => [...floorCalls[1].splice(0,floor)].some(x => x);
-const isGoingDownOutside = () => [...floorCalls[0].splice(0,floor)].some(x => x);
+const isGoingUpInside = () => [...floorCalls[1]].splice(floor-1).some(x => x);
+const isGoingUpOutside = () => [...floorCalls[0]].splice(floor-1).some(x => x);
+const isGoingDownInside = () => [...floorCalls[1]].splice(0,floor).some(x => x);
+const isGoingDownOutside = () => [...floorCalls[0]].splice(0,floor).some(x => x);
 const updateFloorCalls = () => {
   floorCalls = [[false,false,false,false,false,false,false,false,false,false],[false,false,false,false,false,false,false,false,false,false]];
   for(let i=1; i<=10; i++) if(queue.includes(i)) floorCalls[0][i-1] = true;
   elevButtons.forEach((button, i) => { if(button.classList.contains('lid-up')) floorCalls[1][i] = true; });
-  floorCalls[1].reverse();
-  // floorCalls[0][floor-1] = false;
-  // floorCalls[1][floor-1] = false;
-  // queueOutput.innerHTML = queue;
   console.log(floorCalls[0], floorCalls[1]);
 }
 
-// let elevLoop = setInterval(elevFunc, 2500);
+let elevLoop = setInterval(elevFunc, 1000);
 const unloop=()=>{clearInterval(elevLoop)};
