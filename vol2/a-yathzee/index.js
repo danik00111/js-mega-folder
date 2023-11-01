09d3023425a9f12472b91440a7eaabbed641cf28
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-11-01 18:50:49 +0200

    add newlines for readability

--- vol2/a-yathzee/index.js
+++ vol2/a-yathzee/index.js
@@ -97,6 +97,13 @@ process.stdin.on("data", r => {
     break;
     case (input.startsWith('score ')):
       switch(input.slice(6)) {
+        case 'ones\n':   case '1s\n': case '1\n': console.log(`ones, you score ${[...diceCup].filter(x=>x==1).length * 1} points`); process.exit(); break;
+        case 'twos\n':   case '2s\n': case '2\n': console.log(`ones, you score ${[...diceCup].filter(x=>x==2).length * 2} points`); process.exit(); break;
+        case 'threes\n': case '3s\n': case '3\n': console.log(`ones, you score ${[...diceCup].filter(x=>x==3).length * 3} points`); process.exit(); break;
+        case 'fours\n':  case '4s\n': case '4\n': console.log(`ones, you score ${[...diceCup].filter(x=>x==4).length * 4} points`); process.exit(); break;
+        case 'fives\n':  case '5s\n': case '5\n': console.log(`ones, you score ${[...diceCup].filter(x=>x==5).length * 5} points`); process.exit(); break;
+        case 'sixes\n':  case '6s\n': case '6\n': console.log(`ones, you score ${[...diceCup].filter(x=>x==6).length * 6} points`); process.exit(); break;
+
         case 'toak\n':
         case '3oak\n':
         case '3 of a kind\n':
@@ -105,6 +112,7 @@ process.stdin.on("data", r => {
         case 'three of a kind\n':
           console.log(`< 3oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 3oak lul💀'}`); process.exit();
         break;
+
         case 'foak\n':
         case '4oak\n':
         case '4 of a kind\n':
@@ -113,6 +121,7 @@ process.stdin.on("data", r => {
         case 'four of a kind\n':
           console.log(`< 4oak, you score ${(isFOAK()) ? cupSum() : '0 bro fr thought his dice be 4oak lul💀'}`); process.exit();
         break;
+
         case '5oak\n':
         case '5 of a kind\n':
         case '5ofakind\n':
@@ -121,14 +130,17 @@ process.stdin.on("data", r => {
         case 'yz\n': case 'y\n': case 'yathzee\n': case 'yacht\n': case 'yath\n':
           console.log(`< zamnnnnnnn, you scored the yummy ${isYath() ? '50' : '0'} points ZAMNNNNN${isYath() ? '' : '💀💀💀'}`); process.exit();
         break;
+
         case 'fh\n': case 'fullhouse\n': case 'full house\n': case 'house\n':
           console.log(`< fh, you score ${isFH() ? '25' : '0'} points${isFH() ? '' : ' bro fr thought he be having fh lul 💀💀'}`); process.exit();
         break;
+
         case 'straight\n': case 'st\n': case 'str\n': case 'strt\n':
           if(isLgSt()) { console.log(`< 40pts for the large straight \n< (maybe you meant small straight?)`); process.exit(); }
           else if(isShSt()) { console.log(`< 30pts for the small straight \n< (maybe you meant large straight, for whatever reason?)`); process.exit(); }
           else { console.log(`< idk what straight you meant but 0 points any way`); process.exit(); }
         break;
+
         case 'small straight\n': case 'short straight\n':
         case 'smallstraight\n': case 'shortstraight\n':
         case 'smst\n': case 'smstr\n': case 'smstrt\n':
@@ -136,6 +148,7 @@ process.stdin.on("data", r => {
         case 'ss\n':
           console.log(`< small straight, you score ${isShSt() ? '30 pts' : '0 pts brughhhhhh:skyll:💀'}`); process.exit();
         break;
+
         case 'large straight\n': case 'long straight\n':
         case 'largestraight\n': case 'longstraight\n':
         case 'lgst\n': case 'lgstr\n': case 'lgstrt\n':
@@ -143,6 +156,7 @@ process.stdin.on("data", r => {
         case 'ls\n':
           console.log(`< large straight, you score ${isLgSt() ? '40 pts' : '0 pts brughhhhhh:skyll:💀💀'}`); process.exit();
         break;
+
         case 'chance\n': case 'ch\n': case 'sum\n': case 'c\n':
           console.log(`< chance, you got ${cupSum()} points`); process.exit();
         break;
