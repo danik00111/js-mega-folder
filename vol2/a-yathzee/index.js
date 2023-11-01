f239a84e5e4bd8af081176eba048af3a72e63f4e
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-11-01 18:33:10 +0200

    add full house, straights and skull emojis 💀

--- vol2/a-yathzee/index.js
+++ vol2/a-yathzee/index.js
@@ -27,9 +27,7 @@ const roll = () => {
 const rerolle = (h) => {
   if(rerolls < 1) { console.log('< nuh uh'); return; }
   let str = h;
-  if(h === undefined) {
-    h = '11111';
-  }else if(parseInt(str) != str) { console.log('< uhmhmh watthrt'); return; }
+  if(parseInt(str) != str) { console.log('< uhmhmh watthrt'); return; }
   diceCup.forEach((die, i) => {
     // console.log(`< die ${i}, char there ${str.charAt(i)} (${str.charAt(i) === '1'})`);
     if(str.charAt(i) === '1') {
@@ -65,6 +63,30 @@ const isFH = () => {
   const uniqueCounts = new Set(Object.values(counts));
   return uniqueCounts.size === 2 && (uniqueCounts.has(2) && uniqueCounts.has(3));
 }
+const isShSt = () => {
+  let the = [...diceCup].sort((a,b)=>a-b);
+  let count = 1;
+  let found = false;
+  the.forEach((die, i) => {
+    if (die === the[i - 1] + 1) {
+      count++;
+      if (count === 4) { found = true; }
+    } else if (die !== the[i - 1]) { count = 1; }
+  });
+  return found;
+}
+const isLgSt = () => {
+  let the = [...diceCup].sort((a,b)=>a-b);
+  let count = 1;
+  let found = false;
+  the.forEach((die, i) => {
+    if (die === the[i - 1] + 1) {
+      count++;
+      if (count === 4) { found = true; }
+    } else if (die !== the[i - 1]) { count = 1; }
+  });
+  return found;
+}
 const cupSum = () => diceCup.reduce((partialSum, a) => partialSum + a, 0);
 roll();
 process.stdin.on("data", r => {
@@ -81,7 +103,7 @@ process.stdin.on("data", r => {
         case '3ofakind\n':
         case 'threeofakind\n':
         case 'three of a kind\n':
-          console.log(`< 3oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 3oak lul'}`); process.exit();
+          console.log(`< 3oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 3oak lul💀'}`); process.exit();
         break;
         case 'foak\n':
         case '4oak\n':
@@ -89,23 +111,45 @@ process.stdin.on("data", r => {
         case '4ofakind\n':
         case 'fourofakind\n':
         case 'four of a kind\n':
-          console.log(`< 4oak, you score ${(isFOAK()) ? cupSum() : '0 bro fr thought his dice be 4oak lul'}`); process.exit();
+          console.log(`< 4oak, you score ${(isFOAK()) ? cupSum() : '0 bro fr thought his dice be 4oak lul💀'}`); process.exit();
         break;
         case '5oak\n':
         case '5 of a kind\n':
         case '5ofakind\n':
         case 'fiveofakind\n':
         case 'five of a kind\n':
-        case 'yz\n': case 'y\n': case 'yathzee\n': case 'yacht\n': case 'yath\n':
-          console.log(`< zamnnnnnnn, you scored the yummy ${isYath ? '50' : '0'} points ZAMNNNNN`); process.exit();
+          case 'yz\n': case 'y\n': case 'yathzee\n': case 'yacht\n': case 'yath\n':
+            console.log(`< zamnnnnnnn, you scored the yummy ${isYath() ? '50' : '0'} points ZAMNNNNN${isYath() ? '' : '💀💀💀'}`); process.exit();
+          break;
+          case 'fh\n': case 'fullhouse\n': case 'full house\n': case 'house\n':
+          console.log(`< fh, you score ${isFH() ? '25' : '0'} points${isFH() ? '' : ' bro fr thought he be having fh lul 💀💀'}`); process.exit();
+          break;
+        case 'straight\n': case 'st\n': case 'str\n': case 'strt\n':
+          if(isLgSt()) { console.log(`< 40pts for the large straight`); process.exit(); }
+          else if(isShSt()) { console.log(`< 30pts for the small straight`); process.exit(); }
+          else { console.log(`< idk what straight you meant but 0 points any way`); process.exit(); }
         break;
-        case 'wollolllo\n': console.log('< Narwinia.'); rerolls = +Infinity; break;
+        case 'small straight\n': case 'short straight\n':
+        case 'smallstraight\n': case 'shortstraight\n':
+        case 'smst\n': case 'smstr\n': case 'smstrt\n':
+        case 'shst\n': case 'shstr\n': case 'shstrt\n':
+        case 'ss\n':
+          console.log(`< small straight, you score ${isShSt() ? '30 pts' : '0 pts brughhhhhh:skyll:💀'}`);
+          break;
+        case 'large straight\n': case 'long straight\n':
+        case 'largestraight\n': case 'longstraight\n':
+        case 'lgst\n': case 'lgstr\n': case 'lgstrt\n':
+        case 'lgst\n': case 'lgstr\n': case 'lgstrt\n':
+        case 'ls\n':
+          console.log(`< large straight, you score ${isLgSt() ? '40 pts' : '0 pts brughhhhhh:skyll:💀💀'}`);
+        break;
         default: console.log('< Waht¿¿'); break;
       }
-    break;
+      break;
     case (input.startsWith('reroll ')):
       rerolle(input.slice(7)); // example input: "reroll 10010", any missing digits are 
     break;
+    case input === 'wollolllo\n': console.log('< Narwinia.'); rerolls = +Infinity; break; // temporary + dev-only
     default: console.log('< Waht??!¿'); break;
   }
 });
