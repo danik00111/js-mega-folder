c83fb012597dafc6164540d8a1b0144c5bc079f2
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-11-02 20:19:02 +0200

    add logs for scoring a bonus yathzee +... + clean up the code in general + add some and remove some skulls ! if you find any bugs, open an issue, ! or dm me on discord: @deepfrieduser

--- vol2/a-yathzee/index.js
+++ vol2/a-yathzee/index.js
@@ -92,65 +92,79 @@ const bonusYath = () => {
   bonusYaths++;
   let dize = diceCup[0];
   if(scores[dize-1] === null) {
-    console.log(`< You will have to play that in ${dize}s.`)
-    routine(dize-1); return;
+    console.log(`< You will have to play that in ${dize}s for ${5 * (dize+1)} pts.`)
+    newTurn(dize-1); return;
   }
   console.log(`< Play this as a joker anywhere in the bottom sect!`)
 }
-const routine = (x) => {
+const newTurn = (x) => {
   if(scores[x] !== null) { console.log('< you already scored there'); return; }
   if(scores[11] === 50 && isYath()) {
     switch(x) {
-      case 0: case 1: case 2: case 3: case 4: case 5: console.log('< why'); return;
-      case 6: scores[6] = cupSum(); break;
-      case 7: scores[7] = cupSum(); break;
-      case 8: scores[8] = 25; break;
-      case 9: scores[9] = 30; break;
-      case 10: scores[10] = 40; break;
-      case 11: console.log('< wait wh- HOWWWWWWWWWWWWW'); process.exit(69); break;
-      case 12: scores[12] = cupSum(); break;
+      case 0: case 1: case 2: case 3: case 4: case 5:
+        if([...scores].splice(6).includes(null)) {
+          console.log('< why'); return;
+        } else {
+          scores[x] = 5 * (x+1);
+        };
+      return;
+      case 6:
+        scores[x] = cupSum();
+        console.log(`< 3oak, you score ${cupSum()}`);
+      break;
+      case 7:
+        scores[x] = cupSum();
+        console.log(`< 4oak, you score ${cupSum()}`);
+      break;
+      case 8:
+        scores[x] = 25;
+        console.log(`< fh, you score 25 points`);
+        break;
+      case 9:
+        scores[x] = 30;
+        console.log(`< small straight, you score 30 pts}`);
+      break;
+      case 10:
+        scores[x] = 40;
+        console.log(`< small straight, you score 40 pts}`);
+      break;
+      case 11:
+        console.log('< wait wh- HOWWWWWWWWWWWWW');
+        process.exit(69);
+      break;
+      case 12:
+        scores[x] = cupSum();
+        console.log(`< chance, you got ${cupSum()} points`);
+      break;
+      default: console.log('< uhmmhmhgh hugh'); return;
     }
+    return;
   }
   switch(x) {
-    case 0:
-      console.log(`ones, you score ${([...diceCup].filter(x=>x==1).length) * 1} points`);
-      scores[0] = ([...diceCup].filter(x=>x==1).length) * 1;
-    break; case 1:
-      console.log(`twos, you score ${([...diceCup].filter(x=>x==2).length) * 2} points`);
-      scores[1] = ([...diceCup].filter(x=>x==2).length) * 2;
-    break; case 2:
-      console.log(`threes, you score ${([...diceCup].filter(x=>x==3).length) * 3} points`);
-      scores[2] = ([...diceCup].filter(x=>x==3).length) * 3;
-    break; case 3:
-      console.log(`fours, you score ${([...diceCup].filter(x=>x==4).length) * 4} points`);
-      scores[3] = ([...diceCup].filter(x=>x==4).length) * 4;
-    break; case 4:
-      console.log(`fives, you score ${([...diceCup].filter(x=>x==5).length) * 5} points`);
-      scores[4] = ([...diceCup].filter(x=>x==5).length) * 5;
-    break; case 5:
-      console.log(`sixes, you score ${([...diceCup].filter(x=>x==6).length) * 6} points`);
-      scores[5] = ([...diceCup].filter(x=>x==6).length) * 6;
+    case 0: case 1: case 2: case 3: case 4: case 5:
+      console.log(`< ${x+1}s, you score ${([...diceCup].filter(h=>h==(x+1)).length) * (x+1)} points`);
+      scores[x] = ([...diceCup].filter(h=>h==(x+1)).length) * (x+1);
     break; case 6:
-      console.log(`< 3oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 3oak lul💀'}`);
-      scores[6] = isTOAK() ? cupSum() : 0;
+      console.log(`< 3oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 3oak lul'}`);
+      scores[x] = isTOAK() ? cupSum() : 0;
     break; case 7:
-      console.log(`< 4oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 4oak lul💀'}`);
-      scores[7] = isFOAK() ? cupSum() : 0;
+      console.log(`< 4oak, you score ${(isTOAK()) ? cupSum() : '0 bro fr thought his dice be 4oak lul'}`);
+      scores[x] = isFOAK() ? cupSum() : 0;
     break; case 8:
       console.log(`< fh, you score ${isFH() ? '25 points' : '0 points bro fr thought he be having fh lul 💀'}`);
-      scores[8] = isFH() ? 25 : 0;
+      scores[x] = isFH() ? 25 : 0;
     break; case 9:
-      console.log(`< small straight, you score ${isShSt() ? '30 pts' : '0 pts brughhhhhh:skyll:💀💀'}`);
-      scores[9] = isShSt() ? 30 : 0;
+      console.log(`< small straight, you score ${isShSt() ? '30 pts' : '0 pts brughhhhhh💀💀'}`);
+      scores[x] = isShSt() ? 30 : 0;
       break; case 10:
-      console.log(`< large straight, you score ${isLgSt() ? '40 pts' : '0 pts brughhhhhh:skyll:💀💀'}`);
-      scores[10] = isLgSt() ? 40 : 0;
+      console.log(`< large straight, you score ${isLgSt() ? '40 pts' : '0 pts brughhhhhh💀💀💀'}`);
+      scores[x] = isLgSt() ? 40 : 0;
     break; case 11:
-      console.log(`< zamnnnnnnn, you scored the yummy ${isYath() ? '50' : '0'} points ZAMNNNNN${isYath() ? '' : '💀💀💀'}`);
-      scores[11] = isYath() ? 50 : 0;
+      console.log(`< zamnnnnnnn, you scored the yummy ${isYath() ? '50' : '0'} points ZAMNNNNN${isYath() ? '' : '💀💀💀💀'}`);
+      scores[x] = isYath() ? 50 : 0;
     break; case 12:
       console.log(`< chance, you got ${cupSum()} points`);
-      scores[12] = cupSum();
+      scores[x] = cupSum();
     break; default: console.log('< hugh,'); process.exit(420); break;
   }
   if(!(scores.includes(null))) {
@@ -160,7 +174,7 @@ const routine = (x) => {
   };
   roll();
 }
-const theScores = () => { console.log(`< 1s-6s: ${scores[0]}, ${scores[1]}, ${scores[2]}, ${scores[3]}, ${scores[4]}, ${scores[5]},\n< 3oak: ${scores[6]}, 4oak: ${scores[7]}, fullhouse: ${scores[8]},\n< straights: ${scores[9]}, ${scores[10]}, yath: ${scores[11]}, chance: ${scores[12]},\n< totals: ${totalT()} + ${totalB()} ${(totalT() >= 63) ? '(+35 bonus) ' : ''}${(bonusYaths === 0) ? '' : `(+${bonusYaths * 100} bounus) `}= ${totalT() + totalB() + ((totalT() >= 63) ? 35 : 0) + ((bonusYaths === 0) ? 0 : bonusYaths * 100)}`); }
+const theScores = () => { console.log(`< 1s-6s: ${[...scores].splice(0,6)},\n< 3oak: ${scores[6]}, 4oak: ${scores[7]}, fullhouse: ${scores[8]},\n< straights: ${scores[9]}, ${scores[10]}, yath: ${scores[11]}, chance: ${scores[12]},\n< totals: ${totalT()} + ${totalB()} ${(totalT() >= 63) ? '(+35 bonus) ' : ''}${(bonusYaths === 0) ? '' : `(+${bonusYaths * 100} bounus) `}= ${totalT() + totalB() + ((totalT() >= 63) ? 35 : 0) + ((bonusYaths === 0) ? 0 : bonusYaths * 100)}`); }
 process.stdin.on("data", r => {
   let input = String(r).toLowerCase();
   switch(true) {
@@ -183,19 +197,19 @@ process.stdin.on("data", r => {
     break;
     case (input.startsWith('score ')):
       switch(input.slice(6)) {
-        case 'ones\n':   case '1s\n': case '1\n': routine(0); break;
-        case 'twos\n':   case '2s\n': case '2\n': routine(1); break;
-        case 'threes\n': case '3s\n': case '3\n': routine(2); break;
-        case 'fours\n':  case '4s\n': case '4\n': routine(3); break;
-        case 'fives\n':  case '5s\n': case '5\n': routine(4); break;
-        case 'sixes\n':  case '6s\n': case '6\n': routine(5); break;
-        case 'toak\n': case '3oak\n': case '3 of a kind\n': case '3ofakind\n': case 'threeofakind\n': case 'three of a kind\n': routine(6); break;
-        case 'foak\n': case '4oak\n': case '4 of a kind\n': case '4ofakind\n': case 'fourofakind\n': case 'four of a kind\n': routine(7); break;
-        case 'fh\n': case 'fullhouse\n': case 'full house\n': case 'house\n': routine(8); break;
-        case 'small straight\n': case 'short straight\n': case 'smallstraight\n': case 'shortstraight\n': case 'smst\n': case 'smstr\n': case 'smstrt\n': case 'shst\n': case 'shstr\n': case 'shstrt\n': case 'ss\n': routine(9); break;
-        case 'large straight\n': case 'long straight\n': case 'largestraight\n': case 'longstraight\n': case 'lgst\n': case 'lgstr\n': case 'lgstrt\n': case 'ls\n': routine(10); break;
-        case '5oak\n': case '5 of a kind\n': case '5ofakind\n': case 'fiveofakind\n': case 'five of a kind\n': case 'yz\n': case 'y\n': case 'yathzee\n': case 'yacht\n': case 'yath\n': routine(11); break;
-        case 'chance\n': case 'ch\n': case 'sum\n': case 'c\n': routine(12); break;
+        case 'ones\n':   case '1s\n': case '1\n': newTurn(0); break;
+        case 'twos\n':   case '2s\n': case '2\n': newTurn(1); break;
+        case 'threes\n': case '3s\n': case '3\n': newTurn(2); break;
+        case 'fours\n':  case '4s\n': case '4\n': newTurn(3); break;
+        case 'fives\n':  case '5s\n': case '5\n': newTurn(4); break;
+        case 'sixes\n':  case '6s\n': case '6\n': newTurn(5); break;
+        case 'toak\n': case '3oak\n': case '3 of a kind\n': case '3ofakind\n': case 'threeofakind\n': case 'three of a kind\n': newTurn(6); break;
+        case 'foak\n': case '4oak\n': case '4 of a kind\n': case '4ofakind\n': case 'fourofakind\n': case 'four of a kind\n': newTurn(7); break;
+        case 'fh\n': case 'fullhouse\n': case 'full house\n': case 'house\n': newTurn(8); break;
+        case 'small straight\n': case 'short straight\n': case 'smallstraight\n': case 'shortstraight\n': case 'smst\n': case 'smstr\n': case 'smstrt\n': case 'shst\n': case 'shstr\n': case 'shstrt\n': case 'ss\n': newTurn(9); break;
+        case 'large straight\n': case 'long straight\n': case 'largestraight\n': case 'longstraight\n': case 'lgst\n': case 'lgstr\n': case 'lgstrt\n': case 'ls\n': newTurn(10); break;
+        case '5oak\n': case '5 of a kind\n': case '5ofakind\n': case 'fiveofakind\n': case 'five of a kind\n': case 'yz\n': case 'y\n': case 'yathzee\n': case 'yacht\n': case 'yath\n': newTurn(11); break;
+        case 'chance\n': case 'ch\n': case 'sum\n': case 'c\n': newTurn(12); break;
         case 'straight\n': case 'st\n': case 'str\n': case 'strt\n': console.log('< what straight? [score smst] [score lgst]'); break;
         default: console.log('< Waht¿¿'); break;
       }
