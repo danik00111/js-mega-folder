b0b99b9096fa3b63d50ed63fb5b01beb06cbd30d
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-11-02 20:37:43 +0200

    clean up top sect of the scoreboard with a `.join`

--- vol2/a-yathzee/index.js
+++ vol2/a-yathzee/index.js
@@ -174,7 +174,7 @@ const newTurn = (x) => {
   };
   roll();
 }
-const theScores = () => { console.log(`< 1s-6s: ${[...scores].splice(0,6)},\n< 3oak: ${scores[6]}, 4oak: ${scores[7]}, fullhouse: ${scores[8]},\n< straights: ${scores[9]}, ${scores[10]}, yath: ${scores[11]}, chance: ${scores[12]},\n< totals: ${totalT()} + ${totalB()} ${(totalT() >= 63) ? '(+35 bonus) ' : ''}${(bonusYaths === 0) ? '' : `(+${bonusYaths * 100} bounus) `}= ${totalT() + totalB() + ((totalT() >= 63) ? 35 : 0) + ((bonusYaths === 0) ? 0 : bonusYaths * 100)}`); }
+const theScores = () => { console.log(`< 1s-6s: ${[...scores].splice(0,6).join(', ')},\n< 3oak: ${scores[6]}, 4oak: ${scores[7]}, fullhouse: ${scores[8]},\n< straights: ${scores[9]}, ${scores[10]}, yath: ${scores[11]}, chance: ${scores[12]},\n< totals: ${totalT()} + ${totalB()} ${(totalT() >= 63) ? '(+35 bonus) ' : ''}${(bonusYaths === 0) ? '' : `(+${bonusYaths * 100} bounus) `}= ${totalT() + totalB() + ((totalT() >= 63) ? 35 : 0) + ((bonusYaths === 0) ? 0 : bonusYaths * 100)}`); }
 process.stdin.on("data", r => {
   let input = String(r).toLowerCase();
   switch(true) {
