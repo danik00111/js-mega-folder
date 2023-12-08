33582d4c936b9941cd561658b1309636651e9d53
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-12-08 21:04:40 +0200

    idek

--- vol2/c-pop-that/index.node.js
+++ vol2/c-pop-that/index.node.js
@@ -3,16 +3,24 @@ class Game {
   field = [];
   on = false;
   turn = true;
-  get snap() { return `< board: ${this.field}, p${this.turn?1:2} to play, game running: ${this.on}\n` }
+  get snap() { return `< board: ${this.field}, ${'p'+(this.turn?1:2)+' to play'}\n` }
   mainMenu(x) {
     switch(true){
       case x.startsWith('newgame '):
         this.field=x;
-        if(this.field==='newgame square'){this.field=[6,6,6,6,6,6];}
-        else if(this.field==='newgame octagon'){this.field=[3,5,6,6,5,3];}
-        else this.form(this.field.split(' '));
+        switch(this.field) {
+          case('newgame square'):  this.field=[6,6,6,6,6,6]; break;
+          case('newgame octagon'): this.field=[3,5,6,6,5,3]; break;
+          case('newgame circle'):  this.field=[3,5,6,6,5,3]; break;
+          case('newgame nim'):     this.field=[1,3,5,7];     break;
+          default: this.form(this.field.split(' '));
+        }
         if(this.field.length>0)this.on=true;
-      break; default: console.warn('huh');
+      break; case (x.startsWith('eval ')):
+        if (x == 'eval ' || x == 'eval') { console.log('where value') } else {
+          console.log(`< The ${(evaluate(x.split(' ').slice(1).map(y=>parseInt(y)))) ? 'player to move' : 'waiting player'} can force a win.`)
+        };
+      break;
     }
   }
   play(x) {
@@ -31,7 +39,7 @@ class Game {
     this.form(this.field);
     if(this.field.length===0){
       console.log(`P${this.turn?1:2} wins!`);
-      this.on=false; turn = true;
+      this.on=false; this.turn = true;
     }
   }
   form(x) {this.field = x.map(Number).filter(n=>Number.isInteger(n)&&n>0)}
@@ -51,14 +59,14 @@ const helpCmds = `
 <   (i dont actually know if ill be able to code a minimax)
 < [pop [i] [n]] - 
 <   play a move, popping [n] bubbles in the [i]th row (0-indexed).
-< [abort] - stop the game midway.
+< [exit] - exit this node.js.
 < [advhelp [cmd]] - get some more info on a command's syntax
 `
 const advHelp = {
   "advhelp newgame": `< For a custom board, put any amount of numbers, separated by spaces.
-< Square preset is 6 rows of 6, and octagon preset is 3-5-6-6-5-3.`,
+< Square preset is 6 rows of 6, nim preset is 1-3-5-7, and octagon/circle preset is 3-5-6-6-5-3.`,
   "advhelp eval": `< For a custom board, put any amount of numbers, separated by spaces.
-< Square preset is 6 rows of 6, and octagon preset is 3-5-6-6-5-3.`,
+< Square preset is 6 rows of 6, nim preset is 1-3-5-7, and octagon/circle preset is 3-5-6-6-5-3.`,
   "advhelp pop": `< In the argument, put 2 numbers,
 < the first one pointing to a number in a zero-indexed array (0 = first number),
 < and the second one being how much bubbles you want to pop
@@ -68,10 +76,13 @@ const advHelp = {
   "advhelp help": '< rise and shine 💀💀',
 }
 const help = `
-< 2 players take turns on a board of pop it (usually 6 rows of 6 bubbles).
+< TL;DR: This is a misère game of Nim for 2 players.
+
+< 2 players take turns on a board of pop it (often 6 rows of 6 bubbles).
 < on your turn, you pop one or more bubbles
 < (all bubbles you pop must be on one row).
 < whoever has to pop the last bubble loses, and their opponent wins.
+
 < [help cmds] - get the commands to play the game
 `
 console.log('\n< idek run [help]\n');
@@ -87,5 +98,5 @@ process.stdin.on('data',h=>{
     break; case(!game.on):                   game.mainMenu(inp);
     break; case(game.on):                    game.play(inp);
     break; }
-  console.log(game.snap);
+    if(game.on) console.log(game.snap);
 });
