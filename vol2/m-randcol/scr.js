d8c7108d59ba4f051ca14cb9dd8b43b780a73d67
danik00111 <115283667+danik00111@users.noreply.github.com>
2024-03-06 19:17:55 +0200

    so true!

--- vol2/m-randcol/scr.js
+++ vol2/m-randcol/scr.js
@@ -1,3 +1,5 @@
+const rootGet = (x) => getComputedStyle(document.querySelector(':root')).getPropertyValue('--' + x);
+const rootSet = (x, y) => { document.querySelector(':root').style.setProperty('--' + x, y) };
 /**
  * Generates a random number in the range of the two input numbers (inclusive).
  * @param {int} min
@@ -19,6 +21,7 @@ for(let i=0;i<96;i++) {
   elem.classList.add('color');
   let color = randHex();
   elem.style.backgroundColor = color;
+  elem.style.border = `${rgbBrightness(elem.style.backgroundColor)<0.5?'white':'black'} 1px solid`;
   elem.setAttribute("color",color);
   elem.onmouseenter = () => {
     document.getElementById('hex').innerHTML = elem.getAttribute("color");
@@ -30,3 +33,30 @@ for(let i=0;i<96;i++) {
   };
   document.getElementById('palette').appendChild(elem);
 }
+for(let r=0;r<256;r+=51)
+for(let g=0;g<256;g+=51)
+for(let b=0;b<256;b+=51) {
+  let elem = document.createElement("div");
+  elem.classList.add('color');
+  let color = rgbToHex(`rgb(${r},${g},${b})`);
+  elem.style.backgroundColor = color;
+  elem.style.border = `${rgbBrightness(elem.style.backgroundColor)<0.5?'white':'black'} 1px solid`;
+  elem.setAttribute("color",color);
+  elem.onmouseenter = () => {
+    document.getElementById('hex').innerHTML = elem.getAttribute("color");
+    document.getElementById('rgb').innerHTML = rgbFormat(elem.style.backgroundColor);
+    document.getElementById('hex').style.color = elem.getAttribute("color");
+    document.getElementById('rgb').style.color = elem.getAttribute("color");
+    document.getElementById('hex').style.backgroundColor = (rgbBrightness(elem.style.backgroundColor)<0.5)?'white':'black';
+    document.getElementById('rgb').style.backgroundColor = (rgbBrightness(elem.style.backgroundColor)<0.5)?'white':'black';
+  };
+  document.getElementById(`reds${r/51}`).appendChild(elem);
+}
+document.getElementById('size').oninput = () => {
+  document.getElementById('size').style.width = document.getElementById('size').value.length + 'ch'
+  if(document.getElementById('size').style.width == '0ch')
+  document.getElementById('size').style.width = '3ch'
+}
+document.getElementById('size').addEventListener('input',_.debounce(()=>{
+  if(!document.getElementById('size').value=='')rootSet('size',document.getElementById('size').value+'px')
+},250));
