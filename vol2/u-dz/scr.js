c25248433a24e168c322ed998bf1a355d845f2df
danik00111 <115283667+danik00111@users.noreply.github.com>
2024-05-03 20:09:52 +0300

    beuh

--- vol2/u-dz/scr.js
+++ vol2/u-dz/scr.js
@@ -11,43 +11,44 @@ const rl = readline.createInterface({
 const timer = parseInt(await rl.question('seconds: '));
 setTimeout(()=>{console.log('the q')},timer*1e3);
 rl.close(); */
+const delays = 0.5e3;
 const b = document.getElementById('blokk');
 const anim = ()=>{
   setTimeout(() => {
     b.style.width = "500px";
     b.style.backgroundColor = "hsl(45,100%,50%)"
-  }, 0e3);
+  }, delays * 0);
   setTimeout(() => {
     b.style.height = "500px";
     b.style.backgroundColor = "hsl(90,100%,50%)"
-  }, 0.5e3);
+  }, delays * 1);
   setTimeout(() => {
     b.style.width = "250px";
     b.style.left = "250px";
     b.style.backgroundColor = "hsl(135,100%,50%)"
-  }, 1e3);
+  }, delays * 2);
   setTimeout(() => {
     b.style.height = "250px";
     b.style.top = "250px";
     b.style.backgroundColor = "hsl(180,100%,50%)"
-  }, 1.5e3);
+  }, delays * 3);
   setTimeout(() => {
     b.style.width = "500px";
     b.style.left = "0px";
     b.style.backgroundColor = "hsl(225,100%,50%)"
-  }, 2e3);
+  }, delays * 4);
   setTimeout(() => {
     b.style.height = "500px";
     b.style.top = "0px";
     b.style.backgroundColor = "hsl(270,100%,50%)"
-  }, 2.5e3);
+  }, delays * 5);
   setTimeout(() => {
     b.style.width = "250px";
     b.style.backgroundColor = "hsl(315,100%,50%)"
-  }, 3e3);
+  }, delays * 6);
   setTimeout(() => {
     b.style.height = "250px";
     b.style.backgroundColor = "hsl(0,100%,50%)"
-  }, 3.5e3);
+  }, delays * 7);
 }; anim();
-const i1 = setInterval(anim,4e3);
+const i1 = setInterval(anim,delays*8);
